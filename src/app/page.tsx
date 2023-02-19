2add04d105837b1d746ef39fc1e2494cb032186d
Greedwy-Blu <greedwyblu@gmail.com>
2023-02-19 20:19:51 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 import Link from "next/link";
 
-export default function Home() {
+export default  async function Home() {
+  const response = await fetch('https://api.github.com/users/Greedwy-Blu',{
+   // next:{
+   //   revalidate: 30,
+   // },
+   // cache: 'force-cache'
+   // cache: "no-cache"
+  })
+  const user = await response.json()
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Home</h1>
+      <pre>
+        {JSON.stringify(user, null, 2)}
+      </pre>
       <Link href="/dashboard">Dashboard</Link>
     </div>
   );
